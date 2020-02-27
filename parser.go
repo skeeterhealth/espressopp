ffdcd49a8062c583ad0b41a3181190c7ba809ad0
Giuseppe Greco <giuseppe.greco@agamura.com>
2020-02-27 21:46:25 +0000

    feat: add godoc comments

--- parser.go
+++ parser.go
@@ -6,11 +6,17 @@
 
 package espressopp
 
+// Grammar is the set of structural rules that govern the composition of an
+// Espesso++ expression.
 type Grammar struct {
 }
 
+// Parser is the part of an interpreter that attaches meaning by classifying strings
+// of tokens from the input Espresso++ expression as particular non-terminals
+// and by building the parse tree.
 type Parser struct {
 }
 
+// parse parses the Espresso++ expressions in r and returns the resulting grammar.
 func (p *Parser) parse(r io.Reader) Grammar {
 }
