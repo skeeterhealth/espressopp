e8e595344af1fc5756cd8f9c54abe56240439f6e
Giuseppe Greco <giuseppe.greco@agamura.com>
2020-02-28 18:18:34 +0000

    feat: add interface description

--- interpreter.go
+++ interpreter.go
@@ -6,7 +6,18 @@
 
 package espressopp
 
+// Interpreter is the interface implemented by any interpreter that provides
+// functionality for parsing a specific language and getting back the resulting
+// grammar.
 type Interpreter struct {
+    // Accept lets the specified code generator access the functionality
+    // provided by the interpreter. More precisely, the code generator invokes the
+    // interpreter to parse the expressions in the specified reader and get back
+    // the grammar, which is then used to produce the native query into the
+    // specified writer.
 	Accept (CodeGenerator, io.Reader, io.Writer)
+	
+	// Parse parses the expressions in the specified reader and returns the
+	// resulting grammar.
 	Parse(io.Reader) Grammar
 }
