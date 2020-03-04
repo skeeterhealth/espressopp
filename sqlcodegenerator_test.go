6e67ed5ba4d44039bab90f89421ff53681931a05
Giuseppe Greco <giuseppe.greco@agamura.com>
2020-03-04 22:35:59 +0100

    feat: rename TestDataItem to sqlCodeGeneratorDataItem

--- sqlcodegenerator_test.go
+++ sqlcodegenerator_test.go
@@ -15,7 +15,7 @@ import (
 )
 
 // Input-result data.
-type TestDataItem struct {
+type sqlCodeGeneratorTestDataItem struct {
 	input    string // Espresso++ expression
 	result   string // generated sql
 	hasError bool   // code generator returned error
@@ -24,45 +24,45 @@ type TestDataItem struct {
 // Test case for sql generator.
 func TestGenerateSql(t *testing.T) {
 
-	dataItems := []TestDataItem{
+	dataItems := []sqlCodeGeneratorTestDataItem{
 		{"ident eq 10", "ident = 10", false},
 		{"ident eq 'test'", "ident = 'text'", false},
 		{"ident neq 10", "ident <> 10", false},
 		{"ident neq 'test'", "ident <> 'text'", false},
-		
+
 		{"ident is true", "ident = 1", false},
 		{"ident is false", "ident = 0", false},
 		{"ident is null", "ident IS NULL", false},
 		{"ident is not null", "ident IS NOT NULL", false},
 		{"is ident", "ident = 1", false},
 		{"is not ident", "ident = 0", false},
-		
+
 		{"ident gt 10", "ident > 10", false},
 		{"ident gte 10", "ident >= 10", false},
 		{"ident lt 10", "ident < 10", false},
 		{"ident lte 10", "ident <= 10", false},
 		{"ident between 1 and 10", "ident BETWEEN 1 and 10", false},
-		
-		{"ident startswith 'text'", "ident LIKE 'text%'", false},	
-		{"ident endswith 'text'", "ident LIKE '%text'", false},	
+
+		{"ident startswith 'text'", "ident LIKE 'text%'", false},
+		{"ident endswith 'text'", "ident LIKE '%text'", false},
 		{"ident contains 'text'", "ident LIKE '%text%'", false},
-		
-		{"ident contains 'text'", "ident LIKE '%text%'", false},
-		{"ident contains 'text'", "ident LIKE '%text%'", false},
+
 		{"ident contains 'text'", "ident LIKE '%text%'", false},
 		{"ident contains 'text'", "ident LIKE '%text%'", false},
-		
+		{"ident contains 'text'", "ident LIKE '%text%'", false},
+		{"ident contains 'text'", "ident LIKE '%text%'", false},
+
 		{"ident1 startswith 'text' and (ident2 eq 1 or ident2 gt 10)", "ident LIKE 'text%' AND (ident2 = 1 OR ident2 > 10)", false},
 		{"ident1 startswith 'text' or (ident2 gte 1 and ident2 lte 10)", "ident LIKE 'text%' OR (ident2 >= 1 AND ident2 <= 10)", false},
 		{"ident1 startswith 'text' and not (ident2 eq 1 or ident2 gt 10)", "ident LIKE 'text%' AND NOT(ident2 = 1 OR ident2 > 10)", false},
 		{"ident1 startswith 'text' or not (ident2 gte 1 and ident2 lte 10)", "ident LIKE 'text%' OR NOT (ident2 >= 1 AND ident2 <= 10)", false},
-		
+
 		{"ident eq #today", "ident >= TRUNC(SYSDATE) AND ident < TRUNC(SYSDATE) + 1)", false},
 		{"ident lt (#now minus #duration('PT2H'))", "ident < (SYSDATE - INTERVAL) '2' HOUR", false},
 		{"ident lt (#now plus #duration('PT2H'))", "ident < (SYSDATE + INTERVAL) '2' HOUR", false},
 	}
 
-	interpreter := espressopp.NewEspressopp()
+	interpreter := espressopp.NewEspressoppInterpreter()
 	codeGenerator := espressopp.NewSqlCodeGenerator()
 
 	for _, item := range dataItems {
