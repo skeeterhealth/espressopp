5c5eb423576ba77a8661f70c40294217e83ac5a3
Giuseppe Greco <giuseppe.greco@agamura.com>
2020-03-07 18:41:54 +0100

    refactor: improve code structure

--- parser_test.go
+++ parser_test.go
@@ -56,10 +56,6 @@ func emitExpression(e *Expression) string {
 
 	if e.Op != nil {
 		s = fmt.Sprintf(" %s ", *e.Op)
-	} else if e.Not {
-		s = "not "
-	} else if e.Paren != nil {
-		s = *e.Paren
 	} else if e.Equality != nil {
 		s = emitEquality(e.Equality)
 	} else if e.Comparison != nil {
@@ -72,6 +68,8 @@ func emitExpression(e *Expression) string {
 		s = emitMathematics(e.Mathematics)
 	} else if e.Is != nil {
 		s = emitIs(e.Is)
+	} else if e.ParenExpression != nil {
+		s = emitParenExpression(e.ParenExpression)
 	}
 
 	return s
@@ -79,155 +77,41 @@ func emitExpression(e *Expression) string {
 
 // emitEquality renders e.
 func emitEquality(e *Equality) string {
-	var t1 string
-	var t2 string
-
-	if e.Term1.Identifier != nil {
-		t1 = *e.Term1.Identifier
-	} else if e.Term1.Macro != nil {
-		t1 = *e.Term1.Macro
-	} else if e.Term1.Integer != nil {
-		t1 = strconv.Itoa(*e.Term1.Integer)
-	} else if e.Term1.Decimal != nil {
-		t1 = strconv.FormatFloat(*e.Term1.Decimal, 'f', -1, 64)
-	} else if e.Term1.String != nil {
-		t1 = fmt.Sprintf("'%s'", *e.Term1.String)
-	} else if e.Term1.Bool != nil {
-		t1 = strconv.FormatBool(*e.Term1.Bool)
-	}
-
-	if e.Term2.Identifier != nil {
-		t2 = *e.Term2.Identifier
-	} else if e.Term2.Macro != nil {
-		t2 = *e.Term2.Macro
-	} else if e.Term2.Integer != nil {
-		t2 = strconv.Itoa(*e.Term2.Integer)
-	} else if e.Term2.Decimal != nil {
-		t2 = strconv.FormatFloat(*e.Term2.Decimal, 'f', -1, 64)
-	} else if e.Term2.String != nil {
-		t2 = fmt.Sprintf("'%s'", *e.Term2.String)
-	} else if e.Term2.Bool != nil {
-		t2 = strconv.FormatBool(*e.Term2.Bool)
-	}
+	t1 := emitTerm(e.Term1)
+	t2 := emitTerm(e.Term2)
 
 	return fmt.Sprintf("%s %s %s", t1, e.Op, t2)
 }
 
 // emitComparison renders c.
 func emitComparison(c *Comparison) string {
-	var t1 string
-	var t2 string
-
-	if c.Term1.Identifier != nil {
-		t1 = *c.Term1.Identifier
-	} else if c.Term1.Macro != nil {
-		t1 = *c.Term1.Macro
-	} else if c.Term1.Integer != nil {
-		t1 = strconv.Itoa(*c.Term1.Integer)
-	} else if c.Term1.Decimal != nil {
-		t1 = strconv.FormatFloat(*c.Term1.Decimal, 'f', -1, 64)
-	}
-
-	if c.Term2.Identifier != nil {
-		t2 = *c.Term2.Identifier
-	} else if c.Term2.Macro != nil {
-		t2 = *c.Term2.Macro
-	} else if c.Term2.Integer != nil {
-		t2 = strconv.Itoa(*c.Term2.Integer)
-	} else if c.Term2.Decimal != nil {
-		t2 = strconv.FormatFloat(*c.Term2.Decimal, 'f', -1, 64)
-	}
+	t1 := emitTerm(c.Term1)
+	t2 := emitTerm(c.Term2)
 
 	return fmt.Sprintf("%s %s %s", t1, c.Op, t2)
 }
 
 // emitNumericRange renders n.
-func emitNumericRange(n *NumericRange) string {
-	var t1 string
-	var t2 string
-	var t3 string
-
-	if n.Term1.Identifier != nil {
-		t1 = *n.Term1.Identifier
-	} else if n.Term1.Macro != nil {
-		t1 = *n.Term1.Macro
-	} else if n.Term1.Integer != nil {
-		t1 = strconv.Itoa(*n.Term1.Integer)
-	} else if n.Term1.Decimal != nil {
-		t1 = strconv.FormatFloat(*n.Term1.Decimal, 'f', -1, 64)
-	}
-
-	if n.Term2.Identifier != nil {
-		t2 = *n.Term2.Identifier
-	} else if n.Term2.Macro != nil {
-		t2 = *n.Term2.Macro
-	} else if n.Term2.Integer != nil {
-		t2 = strconv.Itoa(*n.Term2.Integer)
-	} else if n.Term2.Decimal != nil {
-		t2 = strconv.FormatFloat(*n.Term2.Decimal, 'f', -1, 64)
-	}
-
-	if n.Term3.Identifier != nil {
-		t3 = *n.Term3.Identifier
-	} else if n.Term3.Macro != nil {
-		t3 = *n.Term3.Macro
-	} else if n.Term3.Integer != nil {
-		t3 = strconv.Itoa(*n.Term3.Integer)
-	} else if n.Term3.Decimal != nil {
-		t3 = strconv.FormatFloat(*n.Term3.Decimal, 'f', -1, 64)
-	}
+func emitNumericRange(nr *NumericRange) string {
+	t1 := emitTerm(nr.Term1)
+	t2 := emitTerm(nr.Term2)
+	t3 := emitTerm(nr.Term3)
 
 	return fmt.Sprintf("%s between %s and %s", t1, t2, t3)
 }
 
 // emitTextualMatching renders t.
-func emitTextualMatching(t *TextualMatching) string {
-	var t1 string
-	var t2 string
+func emitTextualMatching(tm *TextualMatching) string {
+	t1 := emitTerm(tm.Term1)
+	t2 := emitTerm(tm.Term2)
 
-	if t.Term1.Identifier != nil {
-		t1 = *t.Term1.Identifier
-	} else if t.Term1.Macro != nil {
-		t1 = *t.Term1.Macro
-	} else if t.Term1.String != nil {
-		t1 = fmt.Sprintf("'%s'", *t.Term1.String)
-	}
-
-	if t.Term2.Identifier != nil {
-		t2 = *t.Term2.Identifier
-	} else if t.Term2.Macro != nil {
-		t2 = *t.Term2.Macro
-	} else if t.Term2.String != nil {
-		t2 = fmt.Sprintf("'%s'", *t.Term2.String)
-	}
-
-	return fmt.Sprintf("%s %s %s", t1, t.Op, t2)
+	return fmt.Sprintf("%s %s %s", t1, tm.Op, t2)
 }
 
 // emitMathematics renders m.
 func emitMathematics(m *Mathematics) string {
-	var t1 string
-	var t2 string
-
-	if m.Term1.Identifier != nil {
-		t1 = *m.Term1.Identifier
-	} else if m.Term1.Macro != nil {
-		t1 = *m.Term1.Macro
-	} else if m.Term1.Integer != nil {
-		t1 = strconv.Itoa(*m.Term1.Integer)
-	} else if m.Term1.Decimal != nil {
-		t1 = strconv.FormatFloat(*m.Term1.Decimal, 'f', -1, 64)
-	}
-
-	if m.Term2.Identifier != nil {
-		t2 = *m.Term2.Identifier
-	} else if m.Term2.Macro != nil {
-		t2 = *m.Term2.Macro
-	} else if m.Term2.Integer != nil {
-		t2 = strconv.Itoa(*m.Term2.Integer)
-	} else if m.Term2.Decimal != nil {
-		t2 = strconv.FormatFloat(*m.Term2.Decimal, 'f', -1, 64)
-	}
+	t1 := emitTerm(m.Term1)
+	t2 := emitTerm(m.Term2)
 
 	return fmt.Sprintf("%s %s %s", t1, m.Op, t2)
 }
@@ -252,4 +136,79 @@ func emitIs(i *Is) string {
 	}
 
 	return sb.String()
+}
+
+// emitParenExpression renders p.
+func emitParenExpression(pe *ParenExpression) string {
+	var sb strings.Builder
+
+	if pe.Not {
+		sb.WriteString("not ")
+	}
+
+	sb.WriteString("(")
+
+	for _, e := range pe.Expressions {
+		sb.WriteString(emitExpression(e))
+	}
+
+	sb.WriteString(")")
+
+	return sb.String()
+}
+
+// emitMacro renders m.
+func emitMacro(m *Macro) string {
+	var sb strings.Builder
+
+	sb.WriteString(m.Name)
+
+	if m.Args != nil {
+		sb.WriteString("(")
+		var s string
+
+		for _, a := range m.Args {
+			if a.Identifier != nil {
+				s = *a.Identifier
+			} else if a.Macro != nil {
+				s = emitMacro(a.Macro)
+			} else if a.Integer != nil {
+				s = strconv.Itoa(*a.Integer)
+			} else if a.Decimal != nil {
+				s = strconv.FormatFloat(*a.Decimal, 'f', -1, 64)
+			} else if a.String != nil {
+				s = fmt.Sprintf("'%s'", *a.String)
+			} else if a.Bool != nil {
+				s = strconv.FormatBool(*a.Bool)
+			}
+		}
+
+		sb.WriteString(s)
+		sb.WriteString(")")
+	}
+
+	return sb.String()
+}
+
+// emitTerm renders t.
+func emitTerm(t *Term) string {
+	var s string
+
+	if t.Identifier != nil {
+		s = *t.Identifier
+	} else if t.Integer != nil {
+		s = strconv.Itoa(*t.Integer)
+	} else if t.Decimal != nil {
+		s = strconv.FormatFloat(*t.Decimal, 'f', -1, 64)
+	} else if t.String != nil {
+		s = fmt.Sprintf("'%s'", *t.String)
+	} else if t.Bool != nil {
+		s = strconv.FormatBool(*t.Bool)
+	} else if t.Macro != nil {
+		s = emitMacro(t.Macro)
+	} else if t.ParenTerm != nil {
+		s = fmt.Sprintf("(%s)", emitTerm(t.ParenTerm.Term))
+	}
+
+	return s
 }
