7b2b2ccf929c2938bca8be49d0d84a3a9913bcaa
Giuseppe Greco <giuseppe.greco@agamura.com>
2020-03-08 18:09:47 +0100

    feat: add test cases for expressions with maths

--- testdataitem_test.go
+++ testdataitem_test.go
@@ -44,15 +44,20 @@ func getTestDataItems() []testDataItem {
 		{"ident contains 'text'", "ident LIKE '%text%'", false},
 		{"ident contains 'text'", "ident LIKE '%text%'", false},
 
-		{"ident plus 1", "ident + 1", false},
-		{"ident minus 1", "ident - 1", false},
-		{"ident mul 1", "ident * 1", false},
-		{"ident div 1", "ident / 1", false},
-
 		{"ident1 startswith 'text' and (ident2 eq 1 or ident2 gt 10)", "ident LIKE 'text%' AND (ident2 = 1 OR ident2 > 10)", false},
 		{"ident1 startswith 'text' or (ident2 gte 1 and ident2 lte 10)", "ident LIKE 'text%' OR (ident2 >= 1 AND ident2 <= 10)", false},
 		{"ident1 startswith 'text' and not (ident2 eq 1 or ident2 gt 10)", "ident LIKE 'text%' AND NOT(ident2 = 1 OR ident2 > 10)", false},
 		{"ident1 startswith 'text' or not (ident2 gte 1 and ident2 lte 10)", "ident LIKE 'text%' OR NOT (ident2 >= 1 AND ident2 <= 10)", false},
+
+		{"ident1 eq (ident2 plus 1)", "ident1 = (ident2 + 1)", false},
+		{"ident1 eq (ident2 minus 1)", "ident1 = (ident2 - 1)", false},
+		{"ident1 eq (ident2 mul 1)", "ident1 = (ident2 * 1)", false},
+		{"ident1 eq (ident2 div 1)", "ident1 = (ident2 / 1)", false},
+
+		{"ident1 eq ident2 plus 1", "ident1 = ident2 + 1", false},
+		{"ident1 eq ident2 minus 1", "ident1 = ident2 - 1", false},
+		{"ident1 eq ident2 mul 1", "ident1 = ident2 * 1", false},
+		{"ident1 eq ident2 div 1", "ident1 = ident2 / 1", false},
 
 		{"ident eq #today", "ident >= TRUNC(SYSDATE) AND ident < TRUNC(SYSDATE) + 1)", false},
 		{"ident lt (#now minus #duration('PT2H'))", "ident < (SYSDATE - INTERVAL) '2' HOUR", false},
